db1bd8f0e44659bd89ee2dbf70ceefee0a70897f
corti199 <corti1994@gmail.com>
2015-09-28 20:57:32 -0300

    Update HelloWorld.java

--- src/uk/co/placona/helloWorld/HelloWorld.java
+++ src/uk/co/placona/helloWorld/HelloWorld.java
@@ -2,8 +2,43 @@ package uk.co.placona.helloWorld;
 
 public class HelloWorld {
 
-	public String sayHello() {
-		return "Hello World";
+	private int tiempo = 0,hora=0,minuto=0;
+	
+public String obtener(){
+		
+	return "Hola";	
 	}
+	
+	
+	public void reiniciar (){
+		
+		tiempo=0;
+		hora=0;
+		minuto=0;
+	}
+	
+	
+	public void incrementarTiempo (int tiempo){
+		
+		if(minuto <=60){
+			minuto+=1;
+		}else {
+			hora+=1;
+			minuto=0;
+		}
+	}
+	
+	
+	public String mosTiempotrarTiempo (){
+		String horaCompleta="";
+		horaCompleta+=Integer.toString(hora);
+		horaCompleta+=":"+Integer.toString(minuto);
+		
+		
+		return horaCompleta;
+	}
+	
+	
+
 
 }
