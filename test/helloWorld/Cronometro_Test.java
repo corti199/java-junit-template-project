fdf2017329e5159d01be16a57ee82731fd22c21b
corti199 <corti1994@gmail.com>
2015-09-28 21:42:29 -0300

    Update Cronometro_Test.java

--- test/helloWorld/Cronometro_Test.java
+++ test/helloWorld/Cronometro_Test.java
@@ -10,16 +10,51 @@ public class Cronometro_Test {
 	
 	Cronometro c = new Cronometro();
 	
+			@Test
+	public void mostrarTiempo_test() {
+	assertEquals("0 minutos,0 segundos", c.mosTiempotrarTiempo());
+	}
+	
+
+
 	@Test
-	public void test() {
+	public void test_Reiniciar() {
 	c.reiniciar();
 	assertEquals("0 minutos,0 segundos", c.mosTiempotrarTiempo());
 	}
 	
 	@Test
-	public void test2() {
+	public void test_Reiniciar() {
 	c.reiniciar();
 	assertNotEquals("0 minutos,1 segundos", c.mosTiempotrarTiempo());
 	}
+	
+	
+	
+		@Test
+	public void test_incrementar() {
+	c.reiniciar();	
+	c.incrementarTiempo();
+	assertEquals("0 minutos,1 segundos", c.mosTiempotrarTiempo());
+	}
+			@Test
+	public void test_incrementar2() {
+	c.reiniciar();	
+	c.incrementarTiempo();
+	assertEquals("0 minutos,2 segundos", c.mosTiempotrarTiempo());
+	}
+	
+	@Test
+	public void test_incrementar3() {
+	c.incrementarTiempo();
+	assertNotEquals("0 minutos,2 segundos", c.mosTiempotrarTiempo());
+	}
+	
+	
+	@Test
+	public void test_incrementar5000seg() {
+	assertNotEquals("83 minutos,333333333 segundos", c.mosTiempotrarTiempo());
+	}
+
 	
 }
